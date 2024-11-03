a91a24c20588a18145921cd2157e18cc94dac517
Anastasiia <anass.tttazja@gmail.com>
2024-11-03 13:19:23 +0100

    added task four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+// const intputs = form.querySelectorAll("input");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const elements = event.target.elements;
+
+    const info = {
+        email: elements.email.value,
+        password: elements.password.value
+    }
+
+    if (elements.email.value && elements.password.value === "") {
+       return alert('All form fields must be filled in')
+    } else {
+        console.log(info);
+        form.reset();
+    }
+    
+}
